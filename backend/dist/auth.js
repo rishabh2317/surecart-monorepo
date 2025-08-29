2ac057fe253b684af6ea4245b1ebb0487113f8fb
Rishabh Thakur <rishabh@Rishabhs-MacBook-Air.local>
2025-08-30 01:50:09 +0530

    fixeD: src server bycrypt again with byc again

--- backend/dist/auth.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcrypt';
-const SALT_ROUNDS = 10;
-export const hashPassword = async (password) => {
-    return await bcrypt.hash(password, SALT_ROUNDS);
-};
-export const verifyPassword = async (password, hash) => {
-    return await bcrypt.compare(password, hash);
-};
